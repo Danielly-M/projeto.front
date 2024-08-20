d4d83dfab7594252ade629373a735abdb7a4a9b2
Danielly-M <126516153+Danielly-M@users.noreply.github.com>
2024-08-20 15:28:30 -0300

    commit

--- bank.js
+++ bank.js
@@ -1,36 +1,15 @@
-class Cliente{
-    nome;
-    cpf;
-}
 
-class ContaCorrente{
-    agencia;
-    conta;
-    saldo;
-    sacar(valor){ //método passando valor*operação
-        if(this.saldo >= valor){
-            this.saldo -= valor;
-        }
-    }
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-}
-const cliente1 = new Cliente();
+const cliente1 = new Cliente("Ricardo", "00111122233");
+const cliente2 = new Cliente("Roberto", "11122233344");
 
-cliente1.nome = "Ricardo";
-cliente1.cpf = "00011122233";
-cliente1.agencia = 1001;
-cliente1.conta = 44881;
-cliente1.saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
-contaCorrenteRicardo.agencia = 1001;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(50);
-console.log(contaCorrenteRicardo.saldo);
-//console.log(cliente1);
-
-
-
+const conta2 = new ContaCorrente(1002, cliente2);
+conta2._saldo = 0;
+ 
+contaCorrenteRicardo._saldo = 100;
+conta2._saldo = 2;
+console.log(conta2);
